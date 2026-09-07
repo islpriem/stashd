ed8cf8609dd6799b6f1c56909e0a1129177e5801
Felix Triebel <felix@triebel.me>
2026-09-07 13:57:01 +0200

    add sshd to the e2e container

--- dev/e2e/entrypoint.sh
+++ dev/e2e/entrypoint.sh
@@ -10,4 +10,10 @@ fi
 install -d -o munge -g munge -m 0755 /run/munge /var/log/munge /var/lib/munge
 su munge -s /bin/sh -c "/usr/sbin/munged --force"
 
+# The ssh transfer channel needs a server on the receiving side. Both daemons run in this
+# one container, so it talks to itself over ssh, exactly as two hosts would.
+install -d -m 0755 /run/sshd
+[ -f /etc/ssh/ssh_host_ed25519_key ] || ssh-keygen -A >/dev/null
+/usr/sbin/sshd
+
 exec "$@"
